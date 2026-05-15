3c03d2525dcb37707eb8fb33722a6a200a11cd04
Ilya Isaev <ivisaev@itmo.ru>
2026-05-15 21:58:01 +0300

    main: update project

---
+++ src/main/java/ru/itmo/springsecurity/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package ru.itmo.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(title = "OpenAPI", version = "1.0.0"),
+        security = @SecurityRequirement(name = "BearerAuth"))
+@SecurityScheme(
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        name = "BearerAuth")
+@Configuration
+public class OpenApiConfig {
+    
+}
